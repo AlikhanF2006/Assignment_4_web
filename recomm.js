ee3c6ccfa4588f6b3f64fa641fccb3a61236e3b4
Faizrakhman Alikhan <240381@astanait.edu.kz>
2025-10-23 23:49:02 +0500

    Scroll Progress Bar req

--- recomm.js
+++ recomm.js
@@ -290,3 +290,13 @@ $(window).on("scroll load", function () {
     }
   });
 });
+
+
+
+
+$(window).on("scroll", function () {
+  const scrollTop = $(this).scrollTop();
+  const docHeight = $(document).height() - $(window).height();
+  const scrollPercent = (scrollTop / docHeight) * 100;
+  $("#progressBar").css("width", scrollPercent + "%");
+});
